a5840ef6a598201bced82ac411e2dc0c1a3c7c02
= <=>
2020-10-21 21:40:23 -0400

    lock.c bugfix

--- include/lock.h
+++ include/lock.h
@@ -10,9 +10,14 @@ typedef struct _lock_t {
     qid16 lock_list;
 }lock_t;
 
+extern uint32 test_and_set(uint32 *, uint32);
 
 extern int spinlock_count;
 extern int lock_count;
+
+extern void setpark(pid32);
+extern void park();
+extern void unpark(pid32);
 
 extern syscall sl_initlock(sl_lock_t*);
 extern syscall sl_lock(sl_lock_t*);
@@ -22,8 +27,5 @@ extern syscall initlock(lock_t*);
 extern syscall lock(lock_t*);
 extern syscall unlock(lock_t*);
 
-extern void setpark(pid32);
-extern void park();
-extern void unpark(pid32);
 
-extern uint32 test_and_set(uint32 *, uint32);
+
