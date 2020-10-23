2531efb6b0cd88d0dd7b44fc85ff3f90b7576aa4
= <=>
2020-10-23 05:16:43 -0400

    t4

--- system/deadlock.c
+++ system/deadlock.c
@@ -29,7 +29,7 @@ void find_deadlock(){
                 seen[j] = 1;
                 if (P[j] == i){
                     kprintf("enqueueing %d\n",P[j]);
-                    insert(P[j],cycle_origin_list,-P[j]);
+                    enqueue(P[j],cycle_origin_list);
                     count++;
                     break;
                 }
