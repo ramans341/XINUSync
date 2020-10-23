927d376d0f6eb543efdd4595546ef362ca7d82ef
= <=>
2020-10-23 03:23:34 -0400

    t4 bug fix

--- system/deadlock.c
+++ system/deadlock.c
@@ -3,14 +3,14 @@
 void find_deadlock(){
     kprintf("Deadlock called \n");
     int count = 0;
-    int seen[NPROC];
+    int seen[13];
     int i,j = 0;
 
-    for (i = 0; i < NPROC; i++){
+    for (i = 0; i < 13; i++){
         seen[i] = 0;
     }
 
-    for (i = 0; i < NPROC; i++){
+    for (i = 0; i < 13; i++){
         if (seen[i] != 1){
             j = i;
 
