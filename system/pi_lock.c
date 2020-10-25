8e4ba4151b0ab4fc5d9a43f84466251800461b42
= <=>
2020-10-25 17:57:37 -0400

    t6 bug priority

--- system/pi_lock.c
+++ system/pi_lock.c
@@ -16,13 +16,16 @@ syscall pi_lock(pi_lock_t *l){
 
     while (test_and_set(&l->guard,1)==1);
     
+    temp_prio = proctab[currpid].prprio;
+    proctab[currpid].prprio = 30000;
 
     if (l->flag == 0){
         kprintf("%d acq lk\n", currpid);
         l->flag = 1;
         P[currpid] = -1;
         l->guard = 0;
-        
+        proctab[currpid].prprio = temp_prio;
+
     }
     else {
         if (isempty(l->lock_list)){
@@ -36,9 +39,14 @@ syscall pi_lock(pi_lock_t *l){
         
         kprintf("%d enqd\n", currpid);
         enqueue(currpid, l->lock_list);
+
+        proctab[currpid].prprio = temp_prio;
         priority_boosting();
+        temp_prio = proctab[currpid].prprio;
+        proctab[currpid].prprio = 30000;
         pi_setpark(currpid);
         l->guard = 0;
+        proctab[currpid].prprio = temp_prio;
         pi_park();
     }
     l->owner_pid = currpid;
