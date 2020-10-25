5be012d45b832e2fe23cc4bc961d5ad2e7537727
= <=>
2020-10-25 18:54:07 -0400

    t6

--- system/main.c
+++ system/main.c
@@ -20,16 +20,31 @@ process p_lock(pi_lock_t *l){
 	}
 	return OK;
 }
+process p2_lock(pi_lock_t *l,pi_lock_t *m ){
+	uint32 i;
+	for (i=0; i<5; i++){
+		pi_lock(l);
+		run_for_ms(10);
+        pi_lock(m);
+        run_for_ms(1000);
+		pi_unlock(m);
+        run_for_ms(10);	
+        pi_unlock(l);
+	}
+	return OK;
+}
 
 process main(void) {
-    pi_lock_t lock1;
-    pid32 p1,p2,p3;
+    pi_lock_t lock1,lock2;
+    pid32 p1,p2,p3,p4;
 	int i;
 	
     pi_initlock(&lock1);
+    pi_initlock(&lock2);
     p1 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &lock1);
-    p2 = create((void *)p_lock, INITSTK, 2,"nthreads", 1, &lock1);
+    p2 = create((void *)p2_lock, INITSTK, 2,"nthreads", 2, &lock1,&lock2);
     p3 = create((void *)p_lock, INITSTK, 3,"nthreads", 1, &lock1);
+    p4 = create((void *)p_lock, INITSTK, 4,"nthreads", 1, &lock1);
 	kprintf("Created Processes \n");
 
 	resume(p1);
@@ -40,8 +55,11 @@ process main(void) {
     sleepms(10);
 	resume(p3);
 	kprintf("Resumed p3 \n");
+    sleepms(10);
+	resume(p4);
+	kprintf("Resumed p4 \n");
 
-	for (i = 0; i < 3; i++){
+	for (i = 0; i < 4; i++){
 		receive();
 		kprintf("Process %d / %d received \n", i+1, 3);
 	}
