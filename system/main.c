12c6d7aa0ffe047ce192b6ba0cea4468d54837ae
= <=>
2020-10-25 15:56:12 -0400

    t6

--- system/main.c
+++ system/main.c
@@ -24,12 +24,13 @@ process p_lock(pi_lock_t *l){
 
 process main(void) {
     pi_lock_t lock1;
-    pid32 p1,p2;
+    pid32 p1,p2,p3;
 	int i;
 	
     pi_initlock(&lock1);
     p1 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &lock1);
     p2 = create((void *)p_lock, INITSTK, 2,"nthreads", 1, &lock1);
+    p3 = create((void *)p_lock, INITSTK, 3,"nthreads", 1, &lock1);
 	kprintf("Created Processes \n");
 
 	resume(p1);
@@ -37,10 +38,13 @@ process main(void) {
 	sleepms(50);
 	resume(p2);
 	kprintf("Resumed p2 \n");
+    sleepms(50);
+	resume(p3);
+	kprintf("Resumed p3 \n");
 
-	for (i = 0; i < 2; i++){
+	for (i = 0; i < 3; i++){
 		receive();
-		kprintf("Process %d / %d received \n", i, 2);
+		kprintf("Process %d / %d received \n", i+1, 3);
 	}
 	
 	
