4e9aa836cc33a087aa151975ebf9a138f88d35f2
= <=>
2020-10-26 00:36:46 -0400

    timing test

--- system/main.c
+++ system/main.c
@@ -2,74 +2,76 @@
 
 #include <xinu.h>
 
-process increment(uint32 *x, uint32 n, lock_t *mutex){
-	uint32 i, j;	
-	for (i=0; i<n; i++){
-		lock(mutex);
-		(*x)+=1;
-		for (j=0; j<1000; j++);
-		yield();
-		unlock(mutex);
+uint32 get_timestamp(){
+	return ctr1000;
+}
+
+void run_for_ms(uint32 time){
+	uint32 start = proctab[currpid].runtime;
+	while (proctab[currpid].runtime-start < time);
+}
+
+process p_spinlock(sl_lock_t *l){
+	uint32 i;
+	for (i=0; i<5; i++){
+		sl_lock(l);
+		run_for_ms(1000);
+		sl_unlock(l);		
 	}
 	return OK;
 }
-
-process nthreads(uint32 nt, uint32 *x, uint32 n, lock_t *mutex){
-	pid32 pids[nt];
-	int i;
-	for (i=0; i < nt; i++){
-		pids[i] = create((void *)increment, INITSTK, 1,"p", 3, x, n, mutex);
-		if (pids[i]==SYSERR){
-			kprintf("nthreads():: ERROR - could not create process");
-			return SYSERR;
-		}
+	
+process p_lock(lock_t *l){
+	uint32 i;
+	for (i=0; i<5; i++){
+		lock(l);
+		run_for_ms(1000);
+		unlock(l);		
 	}
-	for (i=0; i < nt; i++){
-		if (resume(pids[i]) == SYSERR){
-			kprintf("nthreads():: ERROR - could not resume process");
-			return SYSERR;
-		}
-	}
-	for (i=0; i < nt; i++) receive();
 	return OK;
 }
-
+	
 process	main(void)
 {
-	uint32 x;			// shared variable
-	unsigned nt;			// number of threads cooperating
-	unsigned value = 1000000; 	// target value of variable
 
-	lock_t mutex;  			// lock
+	sl_lock_t	mutex_sl;  		
+	lock_t 		mutex;  		
+	pid32		pid1, pid2;
+	uint32 		timestamp;
 
-	kprintf("\n\n=====       Testing the LOCK w/ sleep&guard         =====\n");
+	kprintf("\n\n=========== TEST 1: spinlock & 2 threads  ===================\n\n");
+ 	sl_initlock(&mutex_sl); 
+	
+	pid1 = create((void *)p_spinlock, INITSTK, 1,"nthreads", 1, &mutex_sl);
+	pid2 = create((void *)p_spinlock, INITSTK, 1,"nthreads", 1, &mutex_sl);
 
-	// 10 threads
-	kprintf("\n\n================= TEST 1 = 10 threads ===================\n");
-	x = 0;	nt = 10;
- 	initlock(&mutex); 
-	resume(create((void *)nthreads, INITSTK, 1,"nthreads", 4, nt, &x, value/nt, &mutex));
-	receive(); 
-	sync_printf("%d threads, n=%d, target value=%d\n", nt, value, x);
-	if (x==value) kprintf("TEST PASSED.\n"); else kprintf("TEST FAILED.\n");
+	timestamp = get_timestamp();
+	
+	resume(pid1);
+	sleepms(500);
+	resume(pid2);		
 
-	// 20 threads
-        kprintf("\n\n================= TEST 2 = 20 threads ===================\n");
-        x = 0;  nt = 20;
- 	initlock(&mutex); 
-        resume(create((void *)nthreads, INITSTK, 1,"nthreads", 4, nt, &x, value/nt, &mutex));
+	receive();
+	receive();
+
+        kprintf("Time = %d ms\n", get_timestamp()-timestamp);
+        
+	kprintf("\n\n=========== TEST 2: lock w/sleep & 2 threads  ===============\n\n");
+	initlock(&mutex);
+
+        pid1 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &mutex);
+        pid2 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &mutex);
+
+        timestamp = get_timestamp();
+
+        resume(pid1);
+        sleepms(500);
+        resume(pid2);
+
         receive();
-	sync_printf("%d threads, n=%d, target value=%d\n", nt, value, x);
-        if (x==value) kprintf("TEST PASSED.\n"); else kprintf("TEST FAILED.\n");
-
-	// 50 threads
-        kprintf("\n\n================= TEST 3 = 50 threads ===================\n");
-        x = 0;  nt = 50;
- 	initlock(&mutex); 
-        resume(create((void *)nthreads, INITSTK, 1,"nthreads", 4, nt, &x, value/nt, &mutex));
         receive();
-	sync_printf("%d threads, n=%d, target value=%d\n", nt, value, x);
-        if (x==value) kprintf("TEST PASSED.\n"); else kprintf("TEST FAILED.\n");
+
+        kprintf("Time = %d ms\n", get_timestamp()-timestamp);
 
 	return OK;
 }
