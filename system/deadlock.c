808093c8413e9b1780d2887f24257075c886c317
= <=>
2020-10-25 20:14:06 -0400

    deadlock testcase

--- system/deadlock.c
+++ system/deadlock.c
@@ -59,7 +59,7 @@ void find_deadlock(){
 
             while(!isempty(detected_cycle_list)){
                 temp = dequeue(detected_cycle_list);
-                kprintf("P%d-",temp);
+                kprintf("P%s-",proctab[temp].prname);
             }               
             kprintf("\n");
         }
